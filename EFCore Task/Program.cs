4dfc3614f698637c4236fdaff7d5be4f8dc7d2f2
ammaralazki <alazkiyasser3@gmail.com>
2026-07-27 16:17:36 +0400

    class User

--- EFCore Task/Program.cs
+++ EFCore Task/Program.cs
@@ -1,5 +1,14 @@
 ﻿namespace EFCore_Task;
 
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)
