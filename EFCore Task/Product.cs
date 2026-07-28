683227b8f8f21c8b3a586fd4222b66c0b404eb6f
ammaralazki <alazkiyasser3@gmail.com>
2026-07-28 14:31:46 +0400

    Product.cs

---
+++ EFCore Task/Product.cs
@@ -0,0 +1,10 @@
+namespace EFCore_Task;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
