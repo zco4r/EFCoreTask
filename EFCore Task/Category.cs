2ddb6e5e26d1ddd06a18ccb7081656a5084f6df5
ammaralazki <alazkiyasser3@gmail.com>
2026-07-28 16:32:30 +0400

    §

--- EFCore Task/Category.cs
+++ EFCore Task/Category.cs
@@ -5,6 +5,5 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; }
 
-    // العلاقة: تصنيف واحد يحتوي على عدة منتجات (1:N)
     public ICollection<Product> Products { get; set; } = new List<Product>();
 }
