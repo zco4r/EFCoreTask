3e06a3ad477196f8d82a76315610174aa35de323
ammaralazki <alazkiyasser3@gmail.com>
2026-07-28 14:32:01 +0400

    Program.cs

--- EFCore Task/Program.cs
+++ EFCore Task/Program.cs
@@ -1,64 +1,7 @@
 ﻿namespace EFCore_Task;
 
 
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
 
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    public ICollection<Product> Products { get; set; } = new List<Product>();
-}
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-}
-
-public class Order
-{
-    public int Id { get; set; }
-    public DateTime OrderDate { get; set; }
-
-    public int UserId { get; set; }
-    public User User { get; set; }
-
-    public Review Review { get; set; }
-
-    public ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
-}
-
-public class OrderProduct
-{
-    public int OrderId { get; set; }
-    public Order Order { get; set; }
-
-    public int ProductId { get; set; }
-    public Product Product { get; set; }
-
-    public int Quantity { get; set; }
-}
-
-public class Review
-{
-    public int Id { get; set; }
-    public int Rating { get; set; }
-    public string Comment { get; set; }
-
-    public int OrderId { get; set; }
-    public Order Order { get; set; }
-}
 
 class Program
 {
