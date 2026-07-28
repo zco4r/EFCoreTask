8cfb265c3702f7acdb8060a4c971bdb1593797d1
ammaralazki <alazkiyasser3@gmail.com>
2026-07-28 14:32:35 +0400

    User.cs

---
+++ EFCore Task/User.cs
@@ -0,0 +1,9 @@
+namespace EFCore_Task;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
