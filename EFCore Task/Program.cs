2fe19b81d2d0a57df718d228c2d03f94363d2391
ammaralazki <alazkiyasser3@gmail.com>
2026-07-29 17:13:31 +0400

    Task2

--- EFCore Task/Program.cs
+++ EFCore Task/Program.cs
@@ -11,13 +11,47 @@ string username = Console.ReadLine();
 Console.Write("Enter Email: ");
 string email = Console.ReadLine();
 
+var NewUser = new User
+{
+    Username = username,
+     Email = email
+};
+
+context.Users.Add(NewUser);
+context.SaveChanges();
+
+Console.WriteLine("User registered and saved to database successfully!");
+
+Console.WriteLine("=== Register ===");
+Console.Write("Enter Username: ");
+string regUsername = Console.ReadLine();
+
+Console.Write("Enter Email: ");
+string regEmail = Console.ReadLine();
+
 var newUser = new User
 {
-    Name = username,
-     Email = email
+    Username = regUsername,
+    Email = regEmail
 };
 
 context.Users.Add(newUser);
 context.SaveChanges();
+Console.WriteLine("User registered successfully!\n");
 
-Console.WriteLine("User registered and saved to database successfully!");
+
+Console.WriteLine("=== Login ===");
+Console.Write("Enter your Email to login: ");
+string loginEmail = Console.ReadLine();
+
+var foundUser = context.Users.FirstOrDefault(u => u.Email == loginEmail);
+
+if (foundUser != null)
+{
+    
+    Console.WriteLine($"Login Successful! Welcome, {foundUser.Username}");
+}
+else
+{
+    Console.WriteLine("Error: User not found or incorrect email.");
+}
